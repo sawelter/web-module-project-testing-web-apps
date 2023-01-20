1447111a93ec04f91fd4ca943de490c759002240
Sawyer <sawyer.welter@gmail.com>
2023-01-19 23:43:27 -0600

    Tests 4-7, Render 1-3 error messages under certain circumstances

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -4,12 +4,15 @@ import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
+
+// Test 1
 test('renders without errors', () => {
     render(
         <ContactForm />
     );
 });
 
+// Test 2
 test('renders the contact form header', () => {
     // arrange
     render(
@@ -25,16 +28,13 @@ test('renders the contact form header', () => {
     expect(header).toHaveTextContent("Contact Form");
 });
 
+// Test 3
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    // assert
+    // arrange
     render(<ContactForm />)
 
-    // act 
     const firstNameField = screen.getByLabelText(/first name*/i);
-
-    const emailField = screen.getByLabelText(/email*/i);
-    userEvent.type(emailField, "ab");
-
+    // act 
     userEvent.type(firstNameField, "abc"); // < 5 characters length
     const errorMessages = await screen.findAllByTestId(/error/i);
 
@@ -43,17 +43,69 @@ test('renders ONE error message if user enters less then 5 characters into first
 
 });
 
-// test('renders THREE error messages if user enters no values into any fields.', async () => {
-//     fail;
-// });
+// Test 4
+test('renders THREE error messages if user enters no values into any fields.', async () => {
+    render(<ContactForm />)
 
-// test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-//     fail;
-// });
+    const submitButton = screen.getByRole('button');
+    userEvent.click(submitButton);
 
-// test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-//     fail;
-// });
+    await waitFor(() => {
+        const errorMessages = screen.queryAllByTestId(/error/i);
+        expect(errorMessages).toHaveLength(3);
+    });
+});
+
+// Test 5
+test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
+    // arrange
+    render(<ContactForm />);
+    const firstNameInput = screen.getByLabelText(/first name*/i);
+    const lastNameInput  = screen.getByLabelText(/last name*/i);
+    const emailInput = screen.getByLabelText(/email*/i);
+
+    // act
+    userEvent.type(firstNameInput, "Grace");
+    userEvent.type(lastNameInput, "Hopper");
+    userEvent.type(emailInput, "gracehopper");
+
+    await waitFor(() => {
+        const errorMessages = screen.queryAllByTestId(/error/i);
+        expect(errorMessages).toHaveLength(1);
+    });
+});
+
+// Test 6
+test('renders ONE error message if user submits without entering email address', async () => {
+    // arrange
+    render(<ContactForm />);
+    const firstNameInput = screen.getByLabelText(/first name*/i);
+    const lastNameInput  = screen.getByLabelText(/last name*/i);
+    const submitButton = screen.getByRole('button');
+    // act
+    userEvent.type(firstNameInput, "Grace");
+    userEvent.type(lastNameInput, "Hopper");
+    userEvent.click(submitButton);
+
+    const errorMessages = await screen.queryAllByTestId("error");
+
+    // assert
+    expect(errorMessages).toHaveLength(1);
+});
+
+// Test 7
+test('renders "email must be a valid email address" if an invalid email is entered', async () => {
+    render(<ContactForm />);
+
+    const emailInput = screen.getByLabelText(/email*/i);
+    userEvent.type(emailInput, "miss.ada.lovelace");
+
+    const errorMessage = await screen.queryByTestId("error");
+
+    expect(errorMessage).toHaveTextContent("email must be a valid email address");
+
+    
+});
 
 // test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
 //     fail;
