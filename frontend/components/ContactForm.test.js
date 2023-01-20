f0f95a0bb76672bd4acc51a1b5b0e89c7e957a2a
Sawyer <sawyer.welter@gmail.com>
2023-01-19 22:55:08 -0600

    Test 1: render w/o error, Test 2: render form header

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -5,37 +5,50 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', () => {
-
+    render(
+        <ContactForm />
+    );
 });
 
 test('renders the contact form header', () => {
+    // arrange
+    render(
+        <ContactForm />
+    );
 
+    // act
+    const header = screen.getByText(/contact form/i);
+
+    // assert
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+    expect(header).toHaveTextContent("Contact Form");
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-
+    fail;
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-
+    fail;
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-
+    fail;
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-
+    fail;
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-
+    fail;
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-
+    fail;
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
-
+    fail;
 });
